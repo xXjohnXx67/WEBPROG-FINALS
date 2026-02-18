d596baa18d248e5ce2e8e0431473e4f6478b4d9c
AleksejHamer <ahamer@student.apc.edu.ph>
2026-02-18 21:24:44 +0800

    eddited guestbook,controller

--- my-profile/backend/src/guestbook.controller.ts
+++ my-profile/backend/src/guestbook.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
 import { GuestbookService } from './guestbook.service';
 
 @Controller('guestbook')
 export class GuestbookController {
   constructor(private readonly service: GuestbookService) {}
 
-  @Get() getAll() { return this.service.findAll(); }
-  @Post() create(@Body() dto: any) { return this.service.create(dto); }
-  @Put(':id') update(@Param('id') id: string, @Body() dto: any) { return this.service.update(id, dto); }
-  @Delete(':id') remove(@Param('id') id: string) { return this.service.delete(id); }
+  @Get() 
+  getAll() { 
+    return this.service.findAll(); 
+  }
+
+  @Post() 
+  create(@Body() dto: any) { 
+    return this.service.create(dto); 
+  }
+
+  @Delete(':id') 
+  remove(@Param('id') id: string) { 
+    return this.service.delete(id); 
+  }
 }
